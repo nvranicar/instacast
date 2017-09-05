b778ed830c2d24430bfe20dc0326bb27c8d08cc2
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-09-05 17:10:20 -0400

    migrated from ember project, added search and reset functionality with DOM manipulation

---
+++ app/fill-in-result.js
@@ -0,0 +1,39 @@
+export default function fillInResult(el, data) {
+  const location = document.createElement('h3');
+  location.setAttribute('class', 'location');
+  location.innerText = `5-day forecast for ${data.city_name}, ${data.state_code}`;
+  el.appendChild(location);
+
+  const grid = document.createElement('div');
+  grid.setAttribute('class', 'grid');
+  el.appendChild(grid);
+
+  data.data.forEach(function(el) {
+    const gridItem = document.createElement('div');
+    gridItem.setAttribute('class', 'grid__item');
+    grid.appendChild(gridItem);
+
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+    gridItem.appendChild(card);
+
+    const split = el.datetime.split('2017-');
+    const day = split[1];
+    const dayEl = document.createElement('h4');
+    dayEl.innerText = day;
+    card.appendChild(dayEl);
+
+    const icon = document.createElement('img');
+    icon.setAttribute('class', 'weather-icon');
+    icon.setAttribute('src', `/images/icons/${el.weather.icon}.png`);
+    card.appendChild(icon);
+
+    const temp = document.createElement('p');
+    temp.innerText = `${el.temp}°F`;
+    card.appendChild(temp);
+
+    const condition = document.createElement('p');
+    condition.innerText = el.weather.description;
+    card.appendChild(condition);
+  });
+}
--- app/index.js
+++ app/index.js
@@ -1,1 +1,21 @@
 import 'whatwg-fetch';
+import fillInResult from './fill-in-result';
+
+const form = document.querySelector('.search-prompt');
+const searchButton = document.querySelector('.search');
+const results = document.querySelector('.results');
+const resetButton = document.querySelector('.reset');
+
+searchButton.addEventListener('click', () => {
+  fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${form.value}&days=5&units=I&key=ee70dbb4c92e416ab321865b456f5fda`)
+  .then(res => res.json()
+  ).then((data) => {
+    form.value = '';
+    results.innerHTML = '';
+    fillInResult(results, data);
+  });
+});
+
+resetButton.addEventListener('click', () => {
+  results.innerHTML = '<h3 class="empty-text">Search a location to get its 5-day weather forecast.</h3>';
+});
